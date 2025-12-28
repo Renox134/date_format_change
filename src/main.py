12478a0024b0bad22252bb0717e091e11be0d4f1
Renox134 <philipkock13@gmail.com>
2025-12-28 11:25:56 +0100

    changed how the date formats can be passed

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import argparse
 
 DEFAULT_INPUT_PATH = "src/input.csv"
 DEFAULT_OUTPUT_PATH = "src/out.csv"
-DEFAULT_REGEX_PATTERN = r"\d\d.\d\d.\d\d\d\d"
+DEFAULT_DATE_FORMAT = "DD%MM%YYYY"
 
 
 def build_arg_parser() -> argparse.ArgumentParser:
@@ -30,11 +30,11 @@ def build_arg_parser() -> argparse.ArgumentParser:
     )
 
     parser.add_argument(
-        "-r", "--regex",
-        default=DEFAULT_REGEX_PATTERN,
+        "-d", "--date_format",
+        default=DEFAULT_DATE_FORMAT,
         help=(
-            "Regex pattern used to identify dates "
-            "(default: \\d\\d.\\d\\d.\\d\\d\\d\\d)"
+            "Date format of the entries that should be changed. The '%' character means that any separator (e.g. ./-,) is allowed."
+            "(default: DD%MM%YYYY"
         )
     )
 
@@ -47,7 +47,7 @@ def main():
 
     input_lines = []
     output_lines = []
-    regex_matcher = re.compile(args.regex)
+    regex_matcher = re.compile(date_format_to_regex(args.date_format))
 
     # read in file
     with open(args.file, "r") as file:
@@ -57,8 +57,10 @@ def main():
     for line in input_lines:
         to_store = line
         for segment in line.split(";"):
-            if regex_matcher.search(segment):
-                to_store = to_store.replace(segment, convert_date(segment))
+            to_replace = regex_matcher.search(segment)
+            if to_replace is not None:
+                to_store = to_store.replace(to_replace.group(), convert_date(to_replace.group(),
+                                                                             args.date_format))
         output_lines.append(to_store)
 
     # write out changed file
@@ -66,18 +68,48 @@ def main():
         file.writelines(output_lines)
 
 
-def convert_date(to_convert: str) -> str:
+def convert_date(to_convert: str, format: str) -> str:
     """
-    Expects a date of type DD/MM/YYYY and transforms it into a
+    Expects a date of type DD%MM%YYYY or YYYY%MM%DD and transforms it into a
     date of type YYYY-MM-DD.
     The separation character of the input doesn't matter.
 
     :param to_convert: The date of type DD/MM/YYYY to convert.
     :type to_convert: str
-    :return: The converted date of type YYYY-MM-DD
+    :param format: The date format, either DD%MM%YYYY or YYYY%MM%DD.
+    :type format: str
+    :return: The date, formated in YYYY-MM-DD.
     :rtype: str
     """
-    return f"{to_convert[6:10]}-{to_convert[3:5]}-{to_convert[:2]}"
+    match format:
+        case "DD%MM%YYYY":
+            return f"{to_convert[6:10]}-{to_convert[3:5]}-{to_convert[:2]}"
+        case "YYYY%MM%DD":
+            return f"{to_convert[:4]}-{to_convert[5:7]}-{to_convert[8:]}"
+        case _:
+            raise ValueError("The provided date format was not recognized. Valid date formats are DD%MM%YYYY or YYYY%MM%DD.")
+
+
+def date_format_to_regex(date_format: str) -> str:
+    """
+    Creates a matching regex for a given date format.
+    
+    :param date_format: The date format of which the regex should be created.
+    :type date_format: str
+    :return: The correctly formated regex.
+    :rtype: str
+    """
+    r = r""
+    for char in date_format:
+        match char:
+            case "D" | "M" | "Y":
+                r += r"\d"
+            case "%" | ".":
+                r += r"."
+            case _:
+                r += char
+    
+    return r
 
 
 if __name__ == "__main__":
